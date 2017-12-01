3b233b9c902437f9cb4020e5c0d831732eb2abf3
Aran Irahan <aranirahan@gmail.com>
2017-12-01 22:22:41 +0700

    Activity yang mengirim objek ke activity lain

--- app/src/main/java/com/ngoding/aran/activtymengirimobjek/MainActivity.java
+++ app/src/main/java/com/ngoding/aran/activtymengirimobjek/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView b;
+    private TextView c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
